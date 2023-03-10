c8848d775aedaa523f01cc498db2dd326df72bce
tonantr <tonantruong@hotmail.com>
2023-03-10 00:18:57 -0500

    complete Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards(arrayName, eventName) {
+    const msg = [];
+    for (let i = 0; i < arrayName.length; i++) {
+        msg.push(`Thank you, ${arrayName[i]}, for the wonderful ${eventName} gift!`);
+    };
+
+    return msg;
+};
+
+function countDown() {
+    let countdown = 10;
+    while (countdown >= 0) {
+        console.log(countdown--);
+    };
+};
+
+
+
+
+
+
